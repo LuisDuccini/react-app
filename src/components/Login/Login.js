cdcd9cbc563affcd315d1cf84ddbcbd23952614a
luisduccini <rm88360@fiap.com.br>
2021-12-03 17:49:26 -0300

    Commit

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,5 @@
-import './Login.css';
-import React from 'react';
+import './Login.css'
+import React from 'react'
 class NameForm extends React.Component {
     constructor(props) {
       super(props);
